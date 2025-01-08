e7572e7a5ba35d00b958fb3b004db1d2fa2eebc4
Michal Puchy <puchy.michal@gmail.com>
2025-01-08 19:17:34 +0100

    Added Sass preprocessor, HMTL webpack plugin, webpack-dev-server. package.json with development and production modes with webpack configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './index.js',
+  entry: './src/index.js', // Entry file
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js', // Name of the output bundle
+    path: path.resolve(__dirname, 'dist'), // Absolute path to the output location
+    clean: true, // Clean the output directory before building
+  },
+  devServer: {
+    static: path.join(__dirname), // Path to static files
+    compress: true, // Enable gzip compression
+    port: 9000,
+    hot: true, // Hot Module Replacement
+    watchFiles: ['src/**/*'], // Change detection
   },
   module: {
     rules: [
@@ -18,9 +27,29 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.scss$/,
+        // Use 'style-loader', 'css-loader', and 'sass-loader' to process SCSS files.
+        // 'sass-loader' compiles SCSS to CSS, 'css-loader' resolves CSS imports,
+        // and 'style-loader' injects CSS into the DOM.
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      { test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
     ],
   },
-  // Set the mode to 'development' for unoptimized builds.
-  // Change to 'production' for optimized builds.
-  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html', // Path to the HTML file in root
+    }),
+  ],
 };
